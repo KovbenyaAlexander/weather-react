c6c51fbb960e56fc8c307f4ad04b0131912c3f4a
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-06-04 21:15:49 +0300

    change chart api

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 } from "./redux/actions/actions";
 import WeatherContainer from "./Components/WeatherContainer/WeatherContainer";
 import Map from "./Components/Map/Map";
-import Chart_ from "./Components/Chart/Chart";
+import Chart from "./Components/Chart/Chart";
 
 function App({
   isShowLoader,
@@ -39,7 +39,7 @@ function App({
       <Form />
       <WeatherContainer />
       <Map />
-      <Chart_ />
+      <Chart />
     </div>
   );
 }
--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,69 +1,48 @@
-import React from "react";
-import { Chart } from "react-charts";
-import ResizableBox from "./ResizableBox";
-import "./styles.css";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
 import { connect } from "react-redux";
-import useDemoConfig from "./useDemoConfig";
 
-function Graph({ weatherInfo }) {
-  // const { data, randomizeData } = useDemoConfig({
-  //   series: 10,
-  // });
-
-  // console.log(data);
-
-  const series = React.useMemo(
-    () => ({
-      showPoints: false,
-    }),
-    []
-  );
-
-  const axes = React.useMemo(
-    () => [
-      {
-        primary: true,
-        type: "time",
-        position: "bottom",
-      },
-      { type: "linear", position: "left" },
-    ],
-    []
-  );
-
+function Chart({ weatherInfo }) {
   if (!weatherInfo) {
     return <p>Loading...</p>;
   }
 
-  const array = weatherInfo.forecast.forecastday[0].hour.map((item, i) => {
-    const time = new Date(item.time).getHours();
+  const data = weatherInfo.forecast.forecastday[1].hour.map((item, i) => {
+    const hour = new Date(item.time).getHours();
     return {
-      primary: time + 1,
-      secondary: item.temp_c,
+      hour: hour,
+      temperature: item.temp_c,
     };
   });
 
-  console.log(array);
-
-  const data = [
-    {
-      // label: "Series 1",
-      data: array,
-    },
-  ];
-
-  if (!weatherInfo) {
-    return <p>Loading...</p>;
-  }
+  // const data = [
+  //   { name: "Page A", uv: 5, pv: 7400, amt: 9400 },
+  //   { name: "Page B", uv: 6, pv: 6400, amt: 8400 },
+  //   { name: "Page C", uv: 7, pv: 5400, amt: 78400 },
+  //   { name: "Page D", uv: 6, pv: 4400, amt: 2400 },
+  //   { name: "Page E", uv: 5, pv: 3400, amt: 2400 },
+  //   { name: "Page F", uv: 4, pv: 2400, amt: 2400 },
+  // ];
 
   return (
-    <>
-      <br />
-      <br />
-      <ResizableBox>
-        <Chart data={data} series={series} axes={axes} tooltip />
-      </ResizableBox>
-    </>
+    <LineChart
+      width={900}
+      height={450}
+      data={data}
+      margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+    >
+      <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="hour" />
+      <YAxis />
+      <Tooltip />
+    </LineChart>
   );
 }
 
@@ -73,4 +52,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Graph);
+export default connect(mapStateToProps)(Chart);
