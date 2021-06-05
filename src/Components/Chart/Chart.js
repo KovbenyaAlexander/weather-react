f0d5458d169a1b3eac1d5f45ce967a7565160227
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-06-05 14:39:05 +0300

    implement the correct calculation of the chart

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -13,35 +13,59 @@ function Chart({ weatherInfo }) {
     return <p>Loading...</p>;
   }
 
-  const data = weatherInfo.forecast.forecastday[1].hour.map((item, i) => {
+  const currentTime = weatherInfo.current.last_updated;
+  const currentHour = new Date(currentTime).getHours();
+
+  const hoursInfo = weatherInfo.forecast.forecastday[1].hour;
+
+  const data = [];
+
+  hoursInfo.forEach((item) => {
     const hour = new Date(item.time).getHours();
-    return {
-      hour: hour,
-      temperature: item.temp_c,
-    };
+    if (hour < currentHour) {
+      data.push({
+        name: hour,
+        pastHours: item.temp_c,
+        XAxis: hour,
+      });
+    }
+    if (hour == currentHour) {
+      data.push({
+        name: hour,
+        pastHours: item.temp_c,
+        futureHours: item.temp_c,
+        XAxis: hour,
+      });
+    }
+    if (hour > currentHour) {
+      data.push({
+        name: hour,
+        futureHours: item.temp_c,
+        XAxis: hour,
+      });
+    }
   });
 
-  // const data = [
-  //   { name: "Page A", uv: 5, pv: 7400, amt: 9400 },
-  //   { name: "Page B", uv: 6, pv: 6400, amt: 8400 },
-  //   { name: "Page C", uv: 7, pv: 5400, amt: 78400 },
-  //   { name: "Page D", uv: 6, pv: 4400, amt: 2400 },
-  //   { name: "Page E", uv: 5, pv: 3400, amt: 2400 },
-  //   { name: "Page F", uv: 4, pv: 2400, amt: 2400 },
-  // ];
+  const item = weatherInfo.forecast.forecastday[1].hour;
+  for (let i = 0; i < item.length; i++) {
+    const hour = new Date(item[i].time).getHours();
+  }
+
+  console.log(data);
 
   return (
     <LineChart
-      width={900}
-      height={450}
+      width={730}
+      height={250}
       data={data}
-      margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
     >
-      <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
-      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-      <XAxis dataKey="hour" />
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
       <YAxis />
       <Tooltip />
+      <Line type="monotone" dataKey="futureHours" stroke="green" />
+      <Line type="monotone" dataKey="pastHours" stroke="grey" />
     </LineChart>
   );
 }
